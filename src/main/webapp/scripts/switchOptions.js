5f292d0431fe09b3bd42e2c2e5891380816f1ebc
s_jar <legoitcomp@gmail.com>
2022-01-22 11:39:59 +0300

    Change method signature

--- src/main/webapp/scripts/switchOptions.js
+++ src/main/webapp/scripts/switchOptions.js
@@ -1,7 +1,7 @@
-function switchOptions(option){
+function switchOptions(toOption){
     const options = document.getElementsByClassName("option")
     for (let i = 0; i < options.length; i++) {
         options[i].style.display = "none";
     }
-    document.getElementById(option).style.display = "block";
+    document.getElementById(toOption).style.display = "block";
 }
